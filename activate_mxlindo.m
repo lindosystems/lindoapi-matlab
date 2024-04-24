0d417cef62b8e55eebba59b4434a3f48c40be448
mkatlihan <mkatlihan@gmail.com>
2024-04-24 23:32:36 +0300

    nix_bins

---
+++ activate_mxlindo.m
@@ -0,0 +1,8 @@
+% Activate mxlindo 
+LINDOAPI_HOME=getenv('LINDOAPI_HOME');
+global MY_LICENSE_FILE
+MY_LICENSE_FILE = '/home/mka/usr/lindoapi/15.0/license/lndapi150.lic';
+path(path,'/home/mka/usr/lindoapi/15.0/bin/linux64')
+%path(path,'C:\LINDOAPI\BIN\WIN64') %% for Windows 64-bit installations
+path(path,'/home/mka/usr/lindoapi/15.0/include')
+path(path,'/home/mka/usr/lindoapi/15.0/matlab'); 
