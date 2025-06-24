11180329437d9a88786382f325dc77a44dad0256
Mustafa Atlihan <mkatlihan@gmail.com>
2025-06-24 23:03:57 +0300

    license

--- activate_mxlindo.m
+++ activate_mxlindo.m
@@ -1,7 +1,7 @@
 % Activate mxlindo 
 LINDOAPI_HOME=getenv('LINDOAPI_HOME');
 global MY_LICENSE_FILE
-MY_LICENSE_FILE = [ LINDOAPI_HOME '/license/lndapi150.lic'];
+MY_LICENSE_FILE = [ LINDOAPI_HOME '/license/lndapi160.lic'];
 path(path,[ LINDOAPI_HOME '/bin/linux64'])
 %path(path,[ LINDOAPI_HOME '/bin/win64'])
 path(path,[ LINDOAPI_HOME '/include'])
